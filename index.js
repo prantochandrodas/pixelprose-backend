191f617e2f2f2376682ea28de6bd0577fc7699ee
pranto <prantochandrodas@gmail.com>
2024-04-27 00:51:26 +0600

    include api

--- index.js
+++ index.js
@@ -20,8 +20,49 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
 
-        const postCollection = client.db('bookCollege').collection('userCollection');
+        const BusCollection = client.db('busdb').collection('busesCollection');
 
+        app.get('/allBus', async (req, res) => {
+            const query = {}
+            const result = await BusCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.get('/searchBus', async (req, res) => {
+
+            const searchValue = req.query;
+            const destination = searchValue.destination;
+            const date = searchValue.date;
+            const bustype = searchValue.bustype;
+            console.log(searchValue);
+            const query = {
+                $and: [
+                    { destination: destination }, { date: date }, {bustype:bustype}
+                ]
+            }
+            const result=await BusCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        // *********************************save buses****************************
+        app.post('/addBus', async (req, res) => {
+            const businfo = req.body;
+            const time = req.body.time;
+            const date = req.body.date;
+            const destination = req.body.destination;
+            const query = {
+                $and: [
+                    { time: time }, { destination: destination }, { date: date }
+                ]
+            }
+            const result = await BusCollection.findOne(query);
+            if (result) {
+                res.send(false)
+            } else {
+                const result2 = await BusCollection.insertOne(businfo);
+                res.send(result2);
+            }
+        })
 
     } finally {
 
